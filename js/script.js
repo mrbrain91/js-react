138ecdfe9cf058a8d35b017a089fdc7fd92ea702
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-04-03 14:24:39 +0500

    start git

---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(10 % 4);
+
+console.log(2 * 4 != '8');
+
+
+const isChecked = true,
+    isClose = false;
+
+
+console.log(isChecked || !isClose);
