66933e03c6590bccfc8aae612da0a24a4450af1c
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-04-11 00:05:51 +0500

    finished lesson 28 DOM documentgetElement

--- js/script.js
+++ js/script.js
@@ -1,40 +1,24 @@
 'use strict';
 
-// let x = 5; alert( x++ ); 
-
-// [ ] + false - null + true // NaN
-
-// console.log(typeof([]+false));// false
-
-// let y = 1; 
-// let x = y = 2; alert(x);  // 2
+const box = document.getElementById('box');
+console.log(box);
 
 
-
-// console.log([] + 1 + 2); // 12
-
-// alert("1"[0]); //1
+const btns = document.getElementsByTagName('button');
+console.log(btns[0]);
 
 
-// console.log(2 && 1 && null && 0 && undefined); // и запинается на лжи
+const circles = document.getElementsByClassName('circle');
+
+console.log(circles);
 
 
+const hearts = document.querySelectorAll('.heart');
 
-// console.log(!!( 1 && 2 ) === (1 && 2)); false chunki !! boolean
+hearts.forEach(item => {
+    console.log(item);
+});
 
-// alert( null || 2 && 3 || 4 ); //3
+const oneHeart = document.querySelector('.heart');
 
-// const	a = [1, 2, 3]; ikkalsi ikki xil yashik
-// const b = [1, 2, 3]; ikkalsi ikki xil yashik
-
-// console.log(a ==b); // false
-
-// alert( +"Infinity" ); // infinity
-
-// console.log("Ёжик" > "яблоко"); // false
-
-// console.log(0 || "" || 2 || undefined || true || falsе); 2
-
-// И запинается на лжи
-// ИЛИ запинается на правде    запинается тухтатади
-
+console.log(oneHeart);
