4ed0ebe375cec60ebdd4fd7a2374f45e7751d218
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-04-10 19:37:21 +0500

    finished lesson 27

--- js/script.js
+++ js/script.js
@@ -1,67 +1,40 @@
 'use strict';
 
-// to string
+// let x = 5; alert( x++ ); 
 
+// [ ] + false - null + true // NaN
 
-// 1) 
+// console.log(typeof([]+false));// false
 
-console.log(typeof(String(null)));
-
-
-// 2) konkonitizatsiya
-
-console.log(typeof(String(5 + '')));
-
-const num = 5;
-
-console.log("https://vk.com/catalog/" + num);
-
-
-//to number 
- 
-// 1) 
-
-console.log(typeof(Number('4')));
-// 2)
-
-console.log(typeof(+'5'));
-
-//3)
-
-console.log(typeof(parseInt("15px", 10)));
-
-
-let answ = +prompt("Hello", "");
-
-//to boolen
-
-// 0, '', null, undefined, NaN; = false, qolganlari true 
-// 1) 
-
-let switcher = null;
-
-if(switcher) {
-    console.log('Working...');
-}
-
-switcher = 1;
-
-if(switcher) {
-    console.log('Working...');
-}
-
-
-// 2) 
-
-console.log(typeof(Boolean('4')));
-
-// 3)
-
-console.log(typeof(!!'4444'));
+// let y = 1; 
+// let x = y = 2; alert(x);  // 2
 
 
 
+// console.log([] + 1 + 2); // 12
+
+// alert("1"[0]); //1
+
+
+// console.log(2 && 1 && null && 0 && undefined); // и запинается на лжи
 
 
 
+// console.log(!!( 1 && 2 ) === (1 && 2)); false chunki !! boolean
+
+// alert( null || 2 && 3 || 4 ); //3
+
+// const	a = [1, 2, 3]; ikkalsi ikki xil yashik
+// const b = [1, 2, 3]; ikkalsi ikki xil yashik
+
+// console.log(a ==b); // false
+
+// alert( +"Infinity" ); // infinity
+
+// console.log("Ёжик" > "яблоко"); // false
+
+// console.log(0 || "" || 2 || undefined || true || falsе); 2
+
+// И запинается на лжи
+// ИЛИ запинается на правде    запинается тухтатади
 
