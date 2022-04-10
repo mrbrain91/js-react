34f9b17cccd027f20f909b73bfba5c64ed1fe1b5
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-04-10 12:35:42 +0500

    finished lesson 23

--- js/script.js
+++ js/script.js
@@ -1,104 +1,34 @@
 'use strict';
 
-// let a = 5,
-//     b = a;
+// let str = "sdffsdf";
 
-// b = b + 5;
+// let strObj = new String(str);
 
-// console.log(b);
-// console.log(a);
-
-// const obj = {
-//     a: 5,
-//     b: 1
-// };
-
-// const copy = obj;
-
-// copy.a = 10;
-
-// console.log(obj);
-// console.log(copy);
-
-// const obj = {
-//     a: 5,
-//     b: 7,
-//     c: {
-//         x: 10,
-//         y: 1
-//     }
-// };
-
-// function copy(mainObj) {
-//     let copyObj = {};
-
-//     let key;
-//     for (key in mainObj) {
-
-//         copyObj[key] = mainObj[key];
-//     }
-
-//     return copyObj;
-// }
-
-// const newCopy = copy(obj);
-
-// newCopy.a = 10;
-// console.log(obj);
-// console.log(newCopy);
-
-// const number1 = {
-//     a: 6,
-//     b: 8,
-//     c: {
-//         x: 89,
-//         y: 12
-//     }
-// };
-
-// const number2 = {
-//     a: 99,
-//     h: 90
-// };
-
-// const clone = Object.assign({}, number2);
-
-// clone.a = 100;
-// clone.d = 32434;
-
-// console.log(number2);
-// console.log(clone);
+// console.log(typeof(str));
+// console.log(typeof(strObj));
 
 
-// const oldArray = ['a', 'b', 'c'];
-// const newArray = oldArray.slice();
+// console.dir([1,2,3]);
 
-// newArray[1] = 'asdas';
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function(){
+        console.log('Hello');
+    }  
+};
 
-// console.log(oldArray);
-// console.log(newArray);
+const john = Object.create(soldier);
 
-// const math = ['logarifm', 'kasr', 'interval'],
-//       eng = ['sintance', 'words', 'grammal'],
-//       all = [...math, ...eng, 'newpred'];
+const john = {
+    health: 100
+};
 
-// console.log(all);
+// john.__proto__ = soldier;
 
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
+// Object.setPrototypeOf(john, soldier);
 
-const num = [2, 5, 7];
+// console.log(john);
 
-log(...num);
+john.sayHello();
 
-const array = ['a', 'b'];
-
-const newAarray = [...array];
-
-
-
-console.log(newAarray);
-console.log(array);
