e80def6784191be208ff5f1bab5871f3a25d4e88
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-04-16 21:08:07 +0500

    practice finished

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 "use strict";
 
+
+// Объём куба через длину ребра a: 125 
+// V = a3
+
+
+
+// Площадь поверхности куба через длину ребра a: 1
+// S = 6a2
+
+
 // Место для первой задачи
+function calculateVolumeAndArea(lengthRib) {
+    if (Number.isInteger(lengthRib) && lengthRib >= 0) {
 
-const userName = "Anton";
+        // return `Объем куба: ${lengthRib*lengthRib*lengthRib}, площадь всей поверхности: ${lengthRib*lengthRib*6}`;
+        console.log(`Объем куба: ${lengthRib*lengthRib*lengthRib}, площадь всей поверхности: ${lengthRib*lengthRib*6}`);
 
-function sayHello(name) {
-    return `privet, ${name}`;
+
+    } else {
+        // return "При вычисления произашло ошибка";
+        console.log("При вычисления произашло ошибка");
+    }
 }
+calculateVolumeAndArea(-15);
 
-sayHello(userName);
 
 
 
@@ -16,35 +32,101 @@ sayHello(userName);
 
 // Место для второй задачи
 
-const number = 4;
-
-function returnNeighboringNumbers(number) {
-    return [number - 1, number, number + 1];
-}
-
-returnNeighboringNumbers(number);
-
-
-// Место для третьей задачи
-
-
-function getMathResult(numberOne, numberTwo) {
-
-    let result = "";
-
-    if (typeof(numberTwo) === 'string' || numberTwo <= 0) {
-        return numberOne;
-    } else {
-        for (let i = 1; i <= numberTwo; i++) {
-
-            if (i > 1) {
-                result += "---";
-            }
-            result += numberOne * i;
-        }
-        return result;
+function getCoupeNumber(seatNumber) {
+    if (typeof(seatNumber) !== 'number' || seatNumber < 0 || !Number.isInteger(seatNumber)) {
+        return "Ошибка. Проверьте правильность введенного номера места";
     }
 
+    if (seatNumber === 0 || seatNumber > 36) {
+        return "Таких мест в вагоне не существует";
+    }
+
+    for (let i = 4; i <= 36; i = i + 4) {
+        if (seatNumber <= i) {
+            return Math.ceil(i / 4);
+        }
+    }
+    // тут очень много вариантов решения, но выбрал один из элегантных :)
 }
 
-console.log(getMathResult(3, 10));
+console.log(getCoupeNumber(5));
+
+
+
+// const hours = Math.floor(minutesTotal / 60);
+// const minutes = minutesTotal % 60;
+// Место для первой задачи
+function getTimeFromMinutes(minutesTotal) {
+    if (typeof(minutesTotal) !== 'number' || minutesTotal < 0 || !Number.isInteger(minutesTotal)) {
+        return "Ошибка, проверьте данные";
+    }
+
+    const hours = Math.floor(minutesTotal / 60);
+    const minutes = minutesTotal % 60;
+
+    let hoursStr = '';
+
+    switch (hours) {
+        case 0:
+            hoursStr = 'часов';
+            break;
+        case 1:
+            hoursStr = 'час';
+            break;
+        case 2:
+        case 3:
+        case 4:
+            hoursStr = 'часа';
+            break;
+        default:
+            hoursStr = 'часов';
+    }
+    return `Это ${hours} ${hoursStr} и ${minutes} минут`;
+}
+
+console.log(getTimeFromMinutes(70));
+
+
+
+function findMaxNumber(a, b, c, d) {
+    // Самое простое - это использовать Math.max :)
+    // А оптимизировать такую проверку мы научимся совсем скоро
+    if (typeof(a) !== 'number' ||
+        typeof(b) !== 'number' ||
+        typeof(c) !== 'number' ||
+        typeof(d) !== 'number') {
+        return 0;
+    } else {
+        return Math.max(a, b, c, d);
+    }
+}
+
+findMaxNumber(1, 5, 6.6, 10.5);
+findMaxNumber(1, 5, '6', '10');
+
+function fib(num) {
+    if (typeof(num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
+        return "";
+    }
+
+    let result = '';
+    let first = 0;
+    let second = 1;
+
+    for (let i = 0; i < num; i++) {
+        if (i + 1 === num) {
+            result += `${first}`;
+            // Без пробела в конце
+        } else {
+            result += `${first} `;
+        }
+
+        let third = first + second;
+        first = second;
+        second = third;
+    }
+
+    return result;
+}
+
+fib(5);
