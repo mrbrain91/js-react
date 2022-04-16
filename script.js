a0089622745c8bfbcbc7fb2a6156df9cd4d6c14b
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-04-16 17:39:27 +0500

    uptrajneniya tsikl

--- script.js
+++ script.js
@@ -1,60 +1,167 @@
 "use strict";
 
-//stikl vnutri tsikl
+// first task 
 
-// for (let i = 0; i < 3; i++) {
-
-//     console.log(i);
-
-//     for (let j = 0; j < 3; j++) {
-//         console.log(j);
+// Место для первой задачи
+// function firstTask() {
+//     for (let i = 5; i <= 10; i++) {
+//         console.log(i);
 //     }
 // }
 
-
-// *
-// **
-// ***
-// ****
-// *****
-// ******
-
-let result = '';
-const lsength = 9;
-
-for (let i = 1; i < lsength; i++) {
-    for (let j = 0; j < i; j++) {
-        result += "*";
-    }
-    result += "\n";
-}
-console.log(result);
+// firstTask();
 
 
-// metki
+// Место для второй задачи
+// function secondTask() {
+
+//     for (let i = 20; i >= 10; i--) {
+//         if (i == 13) {
+//             break;
+//         }
+//         console.log(i);
+//     }
+
+// }
+
+// secondTask();
+
+// Место для третьей задачи
+// function thirdTask() {
+//     // Пишем решение вот тут
+//     metki: for (let i = 2; i <= 10; i++) {
+
+//         if (i % 2 !== 0) {
+//             continue metki;
+//         }
+//         console.log(i);
+//     }
+// }
+
+// thirdTask();
 
 
-// continue metkasi
-// first: for (let i = 0; i < 3; i++) {
-//     console.log(`first level: ${i}`);
-//     for (let j = 0; j < 3; j++) {
-//         console.log(`second level: ${j}`);
-//         for (let k = 0; k < 3; k++) {
-//             if (k === 2) continue first;
-//             console.log(`third level: ${k}`);
+// Цикл, который нужно переписать:
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// function fourthTask() {
+//     let i = 2;
+//     while (i <= 16) {
+//         if (i % 2 === 0) {
+//             continue;
+//         } else {
+//             console.log(i);
+//         }
+//         i++;
+//     }
+// }
+
+// fourthTask();
+
+
+// function fifthTask() {
+//     const arrayOfNumbers = [];
+
+//     for (let i = 5; i <= 10; i++) {
+//         arrayOfNumbers[i - 5] = i;
+//     }
+
+//     console.log(arrayOfNumbers);
+//     return arrayOfNumbers;
+// }
+
+// fifthTask();
+
+
+
+
+// Second exercises
+
+
+
+// Место для первой задачи
+
+// function firstTask() {
+//     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//     const arr = [3, 5, 8, 16, 20, 23, 50];
+//     const result = [];
+
+//     // Пишем решение вот тут
+//     for (let i = 0; i < arr.length; i++) {
+//         result[i] = arr[i];
+//     }
+//     console.log(result);
+//     // Не трогаем
+//     return result;
+// }
+
+// firstTask();
+
+
+
+// // Место для второй задачи
+// function secondTask() {
+//     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//     const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+//     // Пишем решение вот тут
+
+
+//     for (let i = 0; i < data.length; i++) {
+
+//         if (typeof(data[i]) === 'number') {
+//             data[i] = data[i] * 2;
+//         } else if (typeof(data[i]) === 'string') {
+//             data[i] = `${data[i]} - done`;
 //         }
 //     }
+
+//     console.log(data);
+//     // Не трогаем
+//     return data;
 // }
 
-// break metkasi 
+// secondTask();
 
-first: for (let i = 0; i < 3; i++) {
-    console.log(`first level: ${i}`);
-    for (let j = 0; j < 3; j++) {
-        console.log(`second level: ${j}`);
-        for (let k = 0; k < 3; k++) {
-            if (k === 2) break first;
-            console.log(`third level: ${k}`);
-        }
-    }
-}
+
+// // Место для третьей задачи
+// function thirdTask() {
+//     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//     const data = [5, 10, 'Shopping', 20, 'Homework'];
+//     const result = [];
+
+//     // Пишем решение вот тут
+
+//     for (let i = 1; i <= data.length; i++) {
+//         result[i - 1] = data[data.length - i];
+//     }
+
+//     console.log(result);
+//     // Не трогаем
+//     return result;
+// }
+
+// thirdTask();
+
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+
+// console.log(result);
