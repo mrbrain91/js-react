c8b45375b933b9a93e68b82358ec84a578ece86c
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-04-23 05:30:55 +0500

    lesson 45 sobitiya finished

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 "use strict";
+
+const btns = document.querySelectorAll('button'),
+    overlay = document.querySelector('.overlay');
+
+// btn.onclick = function() {
+//     alert('bos');
+// };
+
+
+
+// btn.addEventListener('click', (e) => {
+//     console.log(e.target);
+//     e.target.remove();
+//     // console.log('hover');
+// });
+
+
+
+const deleteElement = (e) => {
+    console.log(e.currentTarget);
+    console.log(e.type);
+
+};
+
+// btn.addEventListener('click', deleteElement);
+// btn.addEventListener('overlay', deleteElement);
+
+
+btns.forEach(btn => {
+    btn.addEventListener('click', deleteElement, { once: true });
+});
+
+
+
+const link = document.querySelector('a');
+
+link.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    // console.log(event.target);
+});
