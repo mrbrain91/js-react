8189b89f220696aabcab4d55c0efe00197ff6267
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-04-25 16:45:56 +0500

    finished lesson 50 Async, defer, dynamic script

--- js/script.js
+++ js/script.js
@@ -1,41 +1,20 @@
 'use strict';
 
-//touchstart
-
-//touchmove
-
-//touchend
-
-//touchenter
-
-//touchleave
-
-//touchcencel
-
-window.addEventListener('DOMContentLoaded', () => {
-    const box = document.querySelector('.box');
-
-    box.addEventListener('touchstart', (e) => {
-        console.log('start');
-        console.log(e.targetTouches);
-    });
-
-    box.addEventListener('touchmove', (e) => {
-        console.log(e.changedTouches[0].pageX);
-    });
-
-    // box.addEventListener('touchend', (e) => {
-    //     console.log('end');
-    // });
-});
-
-//touches
-//targetTouches
-// changedTouches
 
 
 
-// Hummer js
+const p = document.querySelectorAll('p');
+
+console.log(p);
 
 
 
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.body.append(script); 
+}
+
+loadScript("js/test.js");
+loadScript("js/some.js");
