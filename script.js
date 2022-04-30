636d99c9aa3acf064eabb21648108883b51012ed
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-04-30 05:24:34 +0500

    finished 55-56-57 lessons

--- script.js
+++ script.js
@@ -1,19 +1,56 @@
 'use strict';
 
-const box = document.querySelector('.box');
+const birthday = Symbol('birthday');
 
-const newHeight = 0;
-const newWidth = 400;
 
-function changeParams(elem, h, w) {
-    elem.style.height = `${h ?? 200}px`;
-    elem.style.width = `${w ?? 200}px`;
-    elem.innerHtml = h ? ? 200 * ? ? 200;
+const user = {
+    name: 'Alex',
+    surname: 'Smith',
+    [birthday]: '20/04/2021',
+    showMyPublicData: function() {
+        console.log(`${this.name} ${this.surname}`);
+    }
+};
+
+// Object.defineProperty(user, 'birthday', { value: prompt('Date?'), enumerable: true, configurable: true });
+
+
+
+// console.log(Object.getOwnPropertyDescriptor(user, 'birthday'));
+
+
+// console.log(Object.getOwnPropertyDescriptor(Math, 'PI'));
+
+
+Object.defineProperty(user, 'showMyPublicData', { enumerable: false });
+
+
+for (let key in user) {
+    console.log(key);
 }
 
-changeParams(box, newHeight, newWidth);
+console.log(Object.getOwnPropertyDescriptor(user, 'PI'));
 
-// let userName;
-// let userKey;
 
-// console.log(userName ?? userKey ?? 'User');
+Object.defineProperties(user, {
+    name: { writable: false },
+    surname: { writable: false }
+});
+
+
+
+// user.birthday = 'asdas';
+
+
+// Object.defineProperty(user, 'name', { writable: false });
+
+
+// Object.defineProperty(user, 'gender', { value: 'male' });
+
+// console.log(Object.getOwnPropertyDescriptor(user, 'gender'));
+
+// user.name = 'ddsafs';
+
+//writable 
+//enumerable
+//configurable
