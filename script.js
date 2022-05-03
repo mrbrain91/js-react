98832eabacae004065f058020066e9cd9171f033
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-05-03 15:08:33 +0500

    finished lesson 75-funktsiya konstruktor

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 'use strict';
 
-const box = document.querySelector('.box');
+// const num = new Function(3);
 
-let observer = new MutationObserver(mutationRecords => {
-    console.log(mutationRecords);
-});
+// console.log(num);
 
-observer.observe(box, {
-    childList: true
-});
 
-observer.disconnect();
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`Hello ${this.name}`);
+    };
+}
+
+User.prototype.exit = function() {
+    console.log(`polzovatel ${this.name} ushel`);
+};
+
+
+
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
+
+ivan.exit();
+
+
+console.log(ivan);
+console.log(alex);
+
+
+ivan.hello();
+alex.hello();
