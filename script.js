a5aea0261d54a62763b5cfea53c0fa928cf5b489
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-05-04 12:36:57 +0500

    finished lessons 81-mamp, 82-JSON, cloned objects

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const persone = {
+    name: 'Alex',
+    tel: '+74444444',
+    parents: {
+        mom: 'Olga',
+        dad: 'Mike'
+    }
+};
+
+const clone = JSON.parse(JSON.stringify(persone));
+
+clone.parents.mom = 'Ann';
+
+console.log(persone);
+console.log(clone);
