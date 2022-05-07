bd62fa60bb1ededc2499244a9ef33dfe710aed69
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-05-07 23:47:01 +0500

    finished lesson 86-Promise

---
+++ promice.js
@@ -0,0 +1,54 @@
+// console.log('Zapros dannx...');
+
+// const req = new Promise(function(resolve, reject) {
+//     setTimeout(() => {
+//         console.log('Podgotovka...');
+
+//         const product = {
+//             name: 'TV',
+//             price: 2000
+//         };
+
+//         resolve(product);
+
+//     }, 2000);
+// });
+
+// req.then((product) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             product.status = 'order';
+//             resolve(product);
+//             // reject();
+//         }, 2000);
+//     });
+// }).then(data => {
+//     data.modify = true;
+//     return data;
+// }).then((data) => {
+//     console.log(data);
+// }).catch(() => {
+//     console.error('proizashla oshibki...');
+// }).finally(() => {
+//     console.log('Finally');
+// });
+
+
+const test = time => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), time);
+    });
+};
+
+
+// test(1000).then(() => console.log('1000 ms'));
+// test(2000).then(() => console.log('2000 ms'));
+
+
+// Promise.all([test(1000), test(2000)]).then(() => {
+//     console.log('All');
+// });
+
+Promise.race([test(1000), test(2000)]).then(() => {
+    console.log('All');
+});
