6acc9d60e99076ccc19177ceeac121b29751288c
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-06-08 19:28:58 +0500

    finished lesson 95-kak soxranit dannie bez DB. Rabota s localStorage

---
+++ script.js
@@ -0,0 +1,40 @@
+// localStorage.setItem('number', 5);
+
+// localStorage.removeItem('number');
+
+// localStorage.clear();
+
+// console.log(localStorage.getItem('number'));
+
+const checkbox = document.querySelector('#checkbox'),
+    form = document.querySelector('form'),
+    change = document.querySelector('#color');
+
+if (localStorage.getItem('isChecked')) {
+    checkbox.checked = true;
+}
+
+checkbox.addEventListener('change', () => {
+    localStorage.setItem('isChecked', true);
+});
+
+change.addEventListener('click', () => {
+    if (localStorage.getItem('bg') === 'changed') {
+        localStorage.removeItem('bg');
+        form.style.backgroundColor = '#fff';
+
+    } else {
+        localStorage.setItem('bg', 'changed');
+        form.style.backgroundColor = 'red';
+    }
+});
+
+const persone = {
+    name: 'Alex',
+    age: 25
+};
+
+const serializedPersone = JSON.stringify(persone);
+localStorage.setItem('alex', serializedPersone);
+
+console.log(JSON.parse(localStorage.getItem('alex')));
