2253c82b1e74fbb8ad2acdd2124b2fa88819129c
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-06-10 21:56:39 +0500

    finished lesson 101-Priem modul, kak i zachem ego ispolzovat

--- js/script.js
+++ js/script.js
@@ -1,70 +1,24 @@
-// function User(name, age) {
-//     this.name = name;
-//     let userAge = age;
+const app = '123';
 
-//     this.say = function() {
-//         console.log(`Imya polzovatelya: ${this.name}, vozrast: ${userAge}`);
-//     };
+const number = 1;
 
-//     this.getAge = function() {
-//         return userAge;
-//     };
+(function() {
+    let number = 2;
+    console.log(number);
+    console.log(number + 3);
+}());
 
-//     this.setAge = function(age) {
-//         if (typeof age === 'number' && age > 0 && age < 110) {
-//             userAge = age;
-//         } else {
-//             console.log('Nedopustimoe znachenie!');
-//         }
-//     };
+console.log(number);
 
-// }
+const user = (function() {
+    const privat = function() {
+        console.log('I am privat!');
+    };
 
-// const ivan = new User('Ivan', 27);
+    return {
+        sayHello: privat
+    };
 
-// console.log(ivan.name);
-// console.log(ivan.getAge());
+}());
 
-
-// ivan.setAge(30);
-// ivan.setAge(300);
-// console.log(ivan.getAge());
-
-
-// ivan.say();
-
-// CLASS
-
-class User {
-
-    constructor(name, age) {
-        this.name = name;
-        this._age = age;
-    }
-
-    #
-    surname = 'Petrychenko';
-
-    say = () => {
-        console.log(`Imya polzovatelya: ${this.name} ${this.surname}, vozrast: ${this._age}`);
-    }
-
-    get age() {
-        return this._age;
-    }
-
-    set age(age) {
-        if (typeof age === 'number' && age > 0 && age < 110) {
-            this._age = age;
-        } else {
-            console.log('Nedopustimoe znachenie!');
-        }
-    }
-
-}
-
-const ivan = new User('Ivan', 27);
-
-
-
-ivan.say();
+user.sayHello();
