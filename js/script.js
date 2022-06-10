07ee9bb8cd5657ab13862f04b8dec58b59d375d2
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-06-10 18:14:19 +0500

    finished lesson 100-Inkapsulyatsiya

--- js/script.js
+++ js/script.js
@@ -1,16 +1,70 @@
-const persone = {
-    name: 'Alex',
-    age: 25,
+// function User(name, age) {
+//     this.name = name;
+//     let userAge = age;
 
-    get userAge() {
-        return this.age;
-    },
+//     this.say = function() {
+//         console.log(`Imya polzovatelya: ${this.name}, vozrast: ${userAge}`);
+//     };
 
-    set userAge(num) {
-        this.age = num;
+//     this.getAge = function() {
+//         return userAge;
+//     };
+
+//     this.setAge = function(age) {
+//         if (typeof age === 'number' && age > 0 && age < 110) {
+//             userAge = age;
+//         } else {
+//             console.log('Nedopustimoe znachenie!');
+//         }
+//     };
+
+// }
+
+// const ivan = new User('Ivan', 27);
+
+// console.log(ivan.name);
+// console.log(ivan.getAge());
+
+
+// ivan.setAge(30);
+// ivan.setAge(300);
+// console.log(ivan.getAge());
+
+
+// ivan.say();
+
+// CLASS
+
+class User {
+
+    constructor(name, age) {
+        this.name = name;
+        this._age = age;
     }
 
-};
+    #
+    surname = 'Petrychenko';
 
-console.log(persone.userAge = 30);
-console.log(persone.userAge);
+    say = () => {
+        console.log(`Imya polzovatelya: ${this.name} ${this.surname}, vozrast: ${this._age}`);
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(age) {
+        if (typeof age === 'number' && age > 0 && age < 110) {
+            this._age = age;
+        } else {
+            console.log('Nedopustimoe znachenie!');
+        }
+    }
+
+}
+
+const ivan = new User('Ivan', 27);
+
+
+
+ivan.say();
