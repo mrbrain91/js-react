8adb0462a4830105b60e643d4e8a62c773af27a0
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-06-17 00:16:17 +0500

    finished lesson 110-Biblioteka Jquery

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,9 @@ let path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: './js/script.js',
+    entry: './script.js',
     output: {
-        filename: 'bundle.js',
-        path: __dirname + '/js'
+        filename: 'bundle.js'
     },
     watch: true,
 
