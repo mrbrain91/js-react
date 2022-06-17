436967b3366b53a3720420d1e5d73b1168f54735
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-06-17 16:11:02 +0500

    finished lesson 113 - Web Animation API

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-const btn = document.querySelector('.btn'),
-    elem = document.querySelector('.box');
-let pos = 0;
+const btnPhone = document.querySelector('#iphone'),
+    btnMacbook = document.querySelector('#macbook'),
+    images = document.querySelectorAll('img');
 
 
-// function myAnimation() {
-//     let pos = 0;
 
-//     const id = setInterval(frame, 10);
+let phoneAnimation;
 
-//     function frame() {
-//         if (pos == 300) {
-//             clearInterval(id);
-//         } else {
-//             pos++;
-//             elem.style.top = pos + "px";
-//             elem.style.left = pos + 'px';
-//         }
-//     }
-// }
+btnPhone.addEventListener('click', () => {
+    if (!phoneAnimation) {
+        phoneAnimation = images[0].animate([{
+                transform: 'translateY(0) rotate(0)',
+                filter: 'opacity(100%)'
+            },
+            {
+                transform: 'translateY(100px) rotate(180deg)',
+                filter: 'opacity(50%)'
+            },
+            {
+                transform: 'translateY(-100px) rotate(270deg)',
+                filter: 'opacity(75%)'
 
+            },
+            {
+                transform: 'translateY(0) rotate(360deg)',
+                filter: 'opacity(100%)'
 
-function myAnimation() {
-
-    pos++;
-    elem.style.top = pos + "px";
-    elem.style.left = pos + 'px';
-
-    if (pos < 300) {
-        requestAnimationFrame(myAnimation);
+            }
+        ], {
+            duration: 3000,
+            iterations: Infinity
+        });
+    } else if (phoneAnimation.playState === 'paused') {
+        phoneAnimation.play();
+    } else {
+        phoneAnimation.pause();
     }
-}
-
-
-btn.addEventListener('click', () => requestAnimationFrame(myAnimation));
-
-let id = requestAnimationFrame(myAnimation);
-cancelAnimationFrame(id);
+});
